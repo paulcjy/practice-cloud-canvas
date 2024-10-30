2c0f2c5b8f80c390df7550da6a941f5937972c03
paulcjy <paulcjy@naver.com>
2024-10-30 18:08:39 +0900

    second commit

--- packages/server/src/api/index.ts
+++ packages/server/src/api/index.ts
@@ -3,5 +3,5 @@ import { Router } from 'express';
 export const api: Router = Router();
 
 api.get('/hello', (req, res) => {
-    res.status(200).json({ message: 'Hello, world! asdf' });
+    res.status(200).json({ message: 'Hello, world!' });
 });
